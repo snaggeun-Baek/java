8fd3e9db979f65591d3b93d780884d3f04005622
상근 백 <garu4321@cu.ac.kr>
2021-03-29 10:14:17 +0900

    commit Me

---
+++ src/hello.java
@@ -0,0 +1,23 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        boolean bl;
+        byte btMax, btMin;
+        short st;
+        int iMax, iMin;
+        long lMax, IMin;
+        float xMax, xMin;
+        double yMax, yMin;
+        char ch;
+        
+        yMax = Double.MAX_VALUE;
+        yMin = Double.MIN_VALUE;
+        
+        
+		System.out.println( yMax ); 
+		System.out.println( yMin );
+	}
+
+}
